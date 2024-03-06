74ddb98538216bf4d2343cab65975606a07093a3
Spandan Roy <royspandan1@gmail.com>
2024-03-07 03:33:57 +0530

    Better file names

--- create_maps.rs
+++ create_maps.rs
@@ -5,14 +5,27 @@ fn create_maps(path: &Path) {
         let entry = entry.unwrap();
         let path = entry.path();
         if path.is_dir() {
+            if (path.file_name().unwrap() == ".git")
+                || (path.file_name().unwrap() == "maps")
+                || (path.file_name().unwrap() == ".trash")
+            {
+                continue;
+            }
             create_maps(&path);
         } else {
+            if path.file_name().unwrap() == "README.md" {
+                continue;
+            }
             if let Some(ext) = path.extension() {
                 if ext == "md" {
-                    let mapname = path.file_stem().unwrap().to_str().unwrap();
+                    let mapname = path.to_str().unwrap();
                     // Replace all spaces with underscores and all / with
                     // underscores
                     let mapname = mapname.replace(" ", "_").replace("/", "_");
+                    // remove the leading ./
+                    let mapname = &mapname[2..];
+                    // remove the trailing .md
+                    let mapname = &mapname[..mapname.len() - 3];
                     let mapname = format!("maps/{}.html", mapname);
                     let command_str = format!("markmap --no-open -o \"{:?}\" {:?}", mapname, path);
                     println!("{}", command_str);
